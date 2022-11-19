d2d714500ff63e48cc69e252293babd44ff79579
CuteDefense <ngovoxuantruong8@gmail.com>
2022-11-19 19:41:51 +0700

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Stack } from '@mui/system';
+import { Link } from 'react-router-dom';
+
+import { logo } from '../utils/constants';
+import SearchBar from './SearchBar';
+
+const Navbar = () => (
+   <Stack
+      direction="row"
+      alignItems="center"
+      p={2}
+      sx={{ position: 'sticky', background: '#000', justifyContent: 'space-between', top: '0' }}
+   >
+      <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
+         <img src={logo} alt="Logo" height="45px" />
+      </Link>
+
+      <SearchBar />
+   </Stack>
+);
+
+export default Navbar;
